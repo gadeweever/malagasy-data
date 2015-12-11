b299452fce39489b81ed7760ff94a7fbfd89196d
gadeweever <gamaldeweever@gmail.com>
2015-12-10 21:10:39 -0600

    works

--- stress.rb
+++ stress.rb
@@ -11,15 +11,14 @@ rules = []
 # clash check
 # this determines whether or not we should
 # apply the clash rule
-$clash = 0
+$clash = false
 
 # First Syllable Primary
 def firstPrimary(word)
-  current_stress =  word.syllables[0].stress
   word.syllables[0].stress = StressType::PRIMARY
-  if $clash == 1 then
-    if boundaryCheck(word.syllables[1], nil)
-        word.syllables[0].stress = current_stress
+  if $clash then
+    if !boundaryCheck(word.syllables[1], nil)
+        word.syllables[0].stress = StressType::NONE
     end
   end
   return word
@@ -36,12 +35,13 @@ def noStressFinal(word)
     end
   end
 
-  if $clash == 1
+  # check the clash
+  if $clash
     then if !boundaryCheck(word.syllables[word.syllables.count-1], nil)
-  # we found no stress, so stress the last one
     return word
     end
   end
+  # we found no stress, so stress the last one
   word.syllables.last.stress = StressType::PRIMARY
   return word
 end
@@ -50,17 +50,16 @@ end
 # every other syllable should have secondary stress
 # this rule applies from left to right
 def secondaryOther(word)
-    puts word
   word.syllables.each_with_index do |syllable, index|
 
     #stress every other
-    if index % 2 == 0 then
-      current_stress = syllable.stress
+    if index % 2 == 1 then
         if !syllable.hasStress
           syllable.stress = StressType::SECONDARY
-          if $clash == 1 then
+          if $clash then
+
             if !boundaryCheck(word.syllables[index-1], word.syllables[index+1])
-              syllable.stress = current_stress
+              syllable.stress = StressType::NONE
             end
           end
         end
@@ -74,7 +73,7 @@ end
 # asks if we should establish clash checking
 # Takes a word for method hash
 def clashCheck(word)
-  $clash = 1
+  $clash = true
   return word
 end
 
@@ -82,7 +81,7 @@ end
 # no two syllables next to each other should have stress
 # if they return false
 def boundaryCheck(syllable1, syllable2)
-  return checkStress(syllable1) && checkStress(syllable2)
+  return (checkStress(syllable1) && checkStress(syllable2))
 end
 
 # safely checks the stress of a syllable
@@ -125,25 +124,25 @@ end
 
 # ask for each permute
 permutes.each_with_index do |permute, index|
-  #puts "the value of this is: " << $clash.to_s
+
   #we keep trach of each permuation
   results.print index.to_s << ". "
   permute.each_with_index do |m, index|
     results.print m.name.to_s << " "
   end
-    #puts "the value of this is: " << $clash.to_s
+
   results.puts ""
   # choose a word set that corresponds to what this
   # permute will do. Select all the words in this set
   wordsets[index].words.each do |word|
+    # reset clash for next word
+    $clash = false
 
     # call each rule on the word
     permute.each do |rule|
       word = rule.call word
     end
-
-    # reset clash for next word
-    $clash = 0
   end
+  #output results
   wordsets[index].output(results)
 end
